bc6e6c02bc09330445f133e9217334cedb700a95
Weiguang0406 <weiguang0406@hotmail.com>
2023-01-18 14:19:39 -0500

    updates:  1- Add audio folder( please drop sound in it)  2- Add indicator image;  3- added css id for indicators;  4- added div to hold indicator image and player image; put player css inside the div;  5- JS: rewrote the player moves function to make the players move step by step using newly learned skills as per Robert suggested;

--- js/board.js
+++ js/board.js
@@ -1,4 +1,3 @@
-
 /*
 $(".smallbox:even").css("background","#CFA175");
 
@@ -15,36 +14,40 @@ window.onload = function () {
 
   // $("#player1").appendTo("#div0");
   // $("#player2").appendTo("#div0");
-  // $("#player1").css("visibility", "hidden"); // players invisible as default;
-  // $("#player2").css("visibility", "hidden");
- 
+
   let moves = 0;
   const locations = { player1: 0, player2: 0 }; // players start from location 0;
   let playerNewLocation;
   let tempNewLocation; // Added new var to hold the temporary location before checking ladder and snake;
+  let previousPlayer = "player1"; // Weiguang: Added new var to hold previous player, for indicator feature;
+  let stepSound = new Audio("./audio/move-self.mp3"); // Add step sound file
   let isRolling = false; // Rob: added to prevent double/triple rolls for same player
-// rob: locks out user input with disabled attribute for as long as roll animation preventing multiple turns
+  // rob: locks out user input with disabled attribute for as long as roll animation preventing multiple turns
   var buttonElement = document.getElementById("rolldice_button");
-  buttonElement.addEventListener("click", function() { 
-    if(isRolling) return;
+  buttonElement.addEventListener("click", function () {
+    if (isRolling) return;
     isRolling = true;
     buttonElement.setAttribute("disabled", true);
     rollDice();
-    setTimeout(function() {
-        buttonElement.removeAttribute("disabled");
-        isRolling = false;
+    setTimeout(function () {
+      buttonElement.removeAttribute("disabled");
+      isRolling = false;
     }, 1500);
   });
-
 
   const rollDice = () => {
     moves += 1;
     console.log(moves);
     var randomNumber = Math.trunc(Math.random() * 6 + 1);
     var cubeElement = document.getElementById("cube");
+    // Indicate the player's turn
 
-    // let player2NewLocation = randomNumber + locations.player2;
-    console.dir(cubeElement);
+    $(`#${previousPlayer}indicator`).css("visibility", "hidden");
+    moves % 2 === 1
+      ? $(`#player1indicator`).css("visibility", "visible")
+      : $(`#player2indicator`).css("visibility", "visible");
+
+    // console.dir(cubeElement);
     switch (randomNumber) {
       case 1:
         cubeElement.style.WebkitTransform =
@@ -94,7 +97,7 @@ window.onload = function () {
             return 24;
             break;
           case 21:
-            return 17;
+            return 18; // Was wrongly set 17;
             break;
           default:
             return playerNewLocation;
@@ -102,73 +105,114 @@ window.onload = function () {
       };
       // Player moves
       const playerMoves = (player) => {
-        // let message = `${player}'s turn`;
-        // document.getElementById("message").textContent = message;
-        $(`#${player}`).css("visibility", "visible"); // make the user visible before move
+        let message = `${player}'s turn`;
+        document.getElementById("movingmessage").textContent = message;
+        // let player move step by step using 2 different method:
+        // Function: Move forward step by step with step sound:
+        const sleepNow = (delay) =>
+          new Promise((resolve) => setTimeout(resolve, delay));
+        async function moveSelfForward(steps) {
+          for (let i = 1; i <= steps; i++) {
+            await sleepNow(500);
+            $(`#${player}`).appendTo(`#div${locations[`${player}`] + i}`);
+            stepSound.play();
+          }
+        }
+        // FUNCTION: Move backward step by step:
+        const moveSelfBackward = (steps) => {
+          for (let i = 1; i <= steps; i++) {
+            console.log("i: ", i);
+            const moveWithSound = () => {
+              $(`#${player}`).appendTo(`#div${locations[`${player}`] - i}`);
+              stepSound.play();
+            };
+            setTimeout(() => moveWithSound(), 500 * i);
+          }
+        };
+        // Assign values:
+        previousPlayer = player; //Assign the player to be previous owner for next click;
         tempNewLocation = randomNumber + locations[`${player}`];
         playerNewLocation = snakeOrLadder(tempNewLocation); // check snake or ladders and update the new location accordingly
-        playerNewLocation === tempNewLocation
-          ? (document.getElementById(
-              "movingmessage"
-            ).textContent = `${player} rolled dice of ${randomNumber} and moved to ${playerNewLocation}`)
-          : (document.getElementById(
-              "movingmessage"
-            ).textContent = `${player} rolled dice of ${randomNumber},moved to ${tempNewLocation}, then took the ${
-              playerNewLocation > tempNewLocation ? "ladder up" : "snake down"
-            } to ${playerNewLocation}`);
-        console.log(playerNewLocation);
+        // Weiguang commented out:
+        // Displaying detailed message:
+        // playerNewLocation === tempNewLocation
+        //   ? (document.getElementById(
+        //       "movingmessage"
+        //     ).textContent = `${player} rolled dice of ${randomNumber} and moved to ${playerNewLocation}`)
+        //   : (document.getElementById(
+        //       "movingmessage"
+        //     ).textContent = `${player} rolled dice of ${randomNumber},moved to ${tempNewLocation}, then took the ${
+        //       playerNewLocation > tempNewLocation ? "ladder up" : "snake down"
+        //     } to ${playerNewLocation}`);
+        // console.log(playerNewLocation);
+
         if (playerNewLocation < 25) {
-          $(`#${player}`).appendTo(`#div${playerNewLocation}`);
-          //Player 1: Test move after the dice animition}
-          locations[`${player}`] = playerNewLocation;
-        } //Player 1: wining condition;
-        else if (playerNewLocation === 25) {
-          $(`#${player}`).appendTo(`#div${playerNewLocation}`);
-          document.getElementById(
-            "movingmessage"
-          ).textContent = `${player} Won!`;
-          buttonElement.innerText = "Play again";
-          // reload page in 1s after clicking the the button;
-          buttonElement.addEventListener("click", function () {
-            setTimeout(function () {
-              window.location.reload();
-            }, 1000); // perhaps change to 1500 to match others?
-          });
+          // Weiguang rewrite moves: Use promise to make the player move step by step;
+          // When done, write the new player location to the Locations object for next turn;
+          moveSelfForward(randomNumber)
+            .then(() => {
+              locations[`${player}`] = playerNewLocation;
+            })
+            .then(() => {
+              if (playerNewLocation !== tempNewLocation) {
+                $(`#${player}`).appendTo(`#div${playerNewLocation}`);
+              }
+            })
+            .catch((err) => {
+              throw "Error in if condition1";
+            });
+          console.log(player, locations[`${player}`]);
+        } else if (playerNewLocation === 25) {
+          moveSelfForward(randomNumber)
+            .then(() => {
+              document.getElementById(
+                "movingmessage"
+              ).textContent = `${player} Won! Click button to play again!`;
+              buttonElement.innerText = "Play again";
+              // reload page in 1s after clicking the the button;
+              buttonElement.addEventListener("click", function () {
+                setTimeout(function () {
+                  window.location.reload();
+                }, 1000); // perhaps change to 1500 to match others?
+              });
+            })
+            .catch((err) => {
+              throw "Error calculating location in else if condition";
+            });
         } else {
           tempNewLocation = 25 - (randomNumber - (25 - locations[`${player}`]));
           playerNewLocation = snakeOrLadder(tempNewLocation); // check snake or ladders
 
-          playerNewLocation === tempNewLocation
-            ? (document.getElementById(
-                "movingmessage"
-              ).textContent = `${player} rolled dice of ${randomNumber} and moved back to ${playerNewLocation}`)
-            : (document.getElementById(
-                "movingmessage"
-              ).textContent = `${player} rolled dice of ${randomNumber},moved back to ${tempNewLocation}, then took the ${
-                playerNewLocation > tempNewLocation ? "ladder up" : "snake down"
-              } to ${playerNewLocation}`);
-          $(`#${player}`).appendTo(`#div${playerNewLocation}`);
-          locations[`${player}`] = playerNewLocation;
+          moveSelfForward(`${25 - locations[`${player}`]}`)
+            .then(() => {
+              locations[`${player}`] = playerNewLocation;
+            })
+            .then(() => {
+              moveSelfBackward(`${locations[`${player}`] - tempNewLocation}`);
+            })
+            .then(() => {
+              if (playerNewLocation !== tempNewLocation) {
+                $(`#${player}`).appendTo(`#div${playerNewLocation}`);
+              }
+            })
+            .catch((err) => {
+              throw "Error calculating location";
+            });
+          // Weiguang comment out: // Displaying detailed message:
+          // playerNewLocation === tempNewLocation
+          //   ? (document.getElementById(
+          //       "movingmessage"
+          //     ).textContent = `${player} rolled dice of ${randomNumber} and moved back to ${playerNewLocation}`)
+          //   : (document.getElementById(
+          //       "movingmessage"
+          //     ).textContent = `${player} rolled dice of ${randomNumber},moved back to ${tempNewLocation}, then took the ${
+          //       playerNewLocation > tempNewLocation ? "ladder up" : "snake down"
+          //     } to ${playerNewLocation}`);
+          // $(`#${player}`).appendTo(`#div${playerNewLocation}`);
+          // locations[`${player}`] = playerNewLocation;
         }
       };
 
-      // const player2Turn = () => {
-      //   $("#player2").css("visibility", "visible");
-      //   if (player2NewLocation < 25) {
-      //     $("#player2").appendTo(`#div${player2NewLocation}`);
-      //     //Player 2: Test move after the dice animition
-      //     player2PreLocation = player2NewLocation;
-      //   } else if (player2NewLocation === 25) {
-      //     $("#player2").appendTo(`#div${player2NewLocation}`);
-      //     //Player 1: Test move after the dice animition}
-      //     document.querySelector(".header").innerText =
-      //       "Player2 wins! Game over! Refresh the page to play again";
-      //   } else {
-      //     player2NewLocation = 25 - (randomNumber - (25 - player2PreLocation));
-      //     $("#player2").appendTo(`#div${player2NewLocation}`);
-      //     player2PreLocation = player2NewLocation;
-      //   }
-      // };
       moves % 2 === 1 ? playerMoves("player1") : playerMoves("player2");
     }, 1500);
   };
