f468d8adb3270ab39fec27357342ede62c6d21cf
62133399@johnabbottcollege.net <62133399@johnabbottcollege.net>
2023-01-21 22:31:17 -0500

    add comment for passing parameter

--- js/login.js
+++ js/login.js
@@ -10,36 +10,16 @@ window.onload = function () {
         let player1_name = input1;
         let player2_name = input2;
         who_first = document.getElementById("who_first").value;
-        //   alert("who_first=" + who_first);
-        // alert(`"index_snakeboard.html?who_first=${who_first}&player1_name=${player1_name}&player2_name=${player2_name}"`);
-        //  form = document.querySelector("#test_form");
-        // form.action = `index_snakeboard.html?who_first=${who_first}&player1_name=${player1_name}&player2_name=${player2_name}"`
+        if (!player1_name === true) {
+            player1_name = "player1"
+        }
+        if (!player2_name === true) {
+            player2_name = "player2"
+        }
 
-        // alert("lll");
     })
 
 
 
 
 }
-/*
- function postData() {
-    var name = $("#txtName").val();
-    var sex = $("#cboSex").val();
-    var address = $("#txtAddress").val();
-    if (name != "" && sex != 0 && address != "") {
-        var form = document.getElementById("frm");
-        //1./Main/GetData 通过形参接收数据
-        //form.action = "/Main/GetData";
-        //2."/Main/getDataByFormCollection" 通过FormCollection来接收数据
-        form.action = "/Main/getDataByFormCollection";
-        //3.通过 Request.Form["name的属性值"]获取表单数据
-        //form.action = "/Main/getDataByRequest";
-        //4.EntityClass实体类接收数据
-        form.action = "/Main/getDataByEntityClass";
-        form.method = "post";
-        form.submit();
-    } else {
-        alert("表单填写的数据不能为空，请检查");
-    }       
-*/
