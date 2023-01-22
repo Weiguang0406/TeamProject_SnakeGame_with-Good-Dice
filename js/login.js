ece8b24bda69f71e01f118a9301d1146c3fde056
62133399@johnabbottcollege.net <62133399@johnabbottcollege.net>
2023-01-21 23:01:07 -0500

    when input data changed, below name changed together
    when input data changed, below name changed together

--- js/login.js
+++ js/login.js
@@ -4,22 +4,48 @@ window.onload = function () {
     buttonElement.addEventListener("click", function () {
         let input1 = document.getElementById("player1_name").value;
         let input2 = document.getElementById("player2_name").value;
-
-        //   alert("11111111=" + input1);
-        //  alert("22222222=" + input2);
         let player1_name = input1;
         let player2_name = input2;
         who_first = document.getElementById("who_first").value;
-        if (!player1_name === true) {
-            player1_name = "player1"
-        }
-        if (!player2_name === true) {
-            player2_name = "player2"
-        }
+        /*      if (!player1_name === true) {
+                  player1_name = "player1"
+              }
+              if (!player2_name === true) {
+                  player2_name = "player2"
+              }
+      */
+    })
+
+    let player1_inputElement = document.getElementById("player1_name");
+    player1_inputElement.addEventListener("keydown", function () {
+
+        //  alert(document.querySelector("#who_first1_span").innerText);
+        // alert(player1_name.value);
+        document.querySelector("#who_first1_span").innerText = player1_name.value;
+
+    })
+    player1_inputElement.addEventListener("keyup", function () {
+
+        //  alert(document.querySelector("#who_first1_span").innerText);
+        // alert(player1_name.value);
+        document.querySelector("#who_first1_span").innerText = player1_name.value;
 
     })
 
+    let player2_inputElement = document.getElementById("player2_name");
+    player2_inputElement.addEventListener("keydown", function () {
 
+        //  alert(document.querySelector("#who_first1_span").innerText);
+        // alert(player1_name.value);
+        document.querySelector("#who_first2_span").innerText = player2_name.value;
 
+    })
+    player2_inputElement.addEventListener("keyup", function () {
+
+        //  alert(document.querySelector("#who_first1_span").innerText);
+        // alert(player1_name.value);
+        document.querySelector("#who_first2_span").innerText = player2_name.value;
+
+    })
 
 }
